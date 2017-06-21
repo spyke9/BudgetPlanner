efcf4c85370d9c3b5c58f9f105e85244a7a8b02e
szkutek <aga.szkutek@gmail.com>
2017-06-21 20:32:41 +0200

    MainWindow update

--- src/bp/gui/MainWindow.java
+++ src/bp/gui/MainWindow.java
@@ -1,9 +1,12 @@
 package bp.gui;
 
-import bp.gui.addform.AddForm;
+import bp.config.Configuration;
 import bp.gui.planner.PlannerPanel;
 import bp.gui.summary.SummaryPanel;
-import bp.gui.transactions.*;
+import bp.gui.transactions.TransactionsPanel;
+import bp.repository.SummaryRepository;
+import bp.repository.TransactionRepository;
+import bp.services.*;
 
 import javax.swing.*;
 
@@ -12,7 +15,10 @@ import javax.swing.*;
  */
 public class MainWindow extends JFrame {
 
-    public MainWindow() {
+    public MainWindow(TransactionService transactionService,
+                      SummaryService summaryService,
+                      GraphService graphService,
+                      BudgetPlanner budgetPlanner) {
 
         TransactionsPanel transactionpanel = new TransactionsPanel();
         PlannerPanel plannerPanel = new PlannerPanel();
@@ -30,9 +36,7 @@ public class MainWindow extends JFrame {
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setSize(800, 600);
         this.setResizable(false);
-//      setLayout(new BorderLayout());
 
-//        add(panel,BorderLayout.NORTH);
         add(table);
         this.setVisible(true);
     }
@@ -40,13 +44,36 @@ public class MainWindow extends JFrame {
 
     public static void main(String[] args) {
 
-        MainWindow window = new MainWindow();
+        TransactionRepository transactionRepository = null;
+        SummaryRepository summaryRepository = null;
 
-        //  AddForm addForm = new AddForm();
+        try {
+            transactionRepository = (TransactionRepository) Serializer.deserialize(Configuration.TRANSACTION_REPOSITORY_FILE);
+            summaryRepository = (SummaryRepository) Serializer.deserialize(Configuration.SUMMARY_REPOSITORY_FILE);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
-//        PieChart demo = new PieChart("Comparison", "Which operating system are you using?");
-//        demo.pack();
-//        demo.setVisible(true);
+        if (transactionRepository == null) {
+            transactionRepository = new TransactionRepository();
+        }
+        if (summaryRepository == null) {
+            summaryRepository = new SummaryRepository();
+        }
+
+        TransactionService transactionService = new TransactionService(transactionRepository);
+        SummaryService summaryService = new SummaryService(summaryRepository);
+        GraphService graphService = new GraphService(summaryRepository);
+        BudgetPlanner budgetPlanner = new BudgetPlanner(summaryRepository);
+
+//        repository.addItem(LocalDate.now(), new Summary());
+//        Serializer.serialize(repository, Configuration.SUMMARY_REPOSITORY_FILE);
+
+
+        MainWindow window = new MainWindow(transactionService, summaryService, graphService, budgetPlanner);
+
+        Serializer.serialize(transactionRepository, Configuration.TRANSACTION_REPOSITORY_FILE);
+        Serializer.serialize(summaryRepository, Configuration.SUMMARY_REPOSITORY_FILE);
 
     }
 
