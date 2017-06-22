97e7470aad7fd2e15ba5ba223f00ac9a224aaa7b
szkutek <aga.szkutek@gmail.com>
2017-06-22 11:19:58 +0200

    MainWindow update

--- src/bp/gui/MainWindow.java
+++ src/bp/gui/MainWindow.java
@@ -4,11 +4,15 @@ import bp.config.Configuration;
 import bp.gui.planner.PlannerPanel;
 import bp.gui.summary.SummaryPanel;
 import bp.gui.transactions.TransactionsPanel;
+import bp.model.CategoryExpensesType;
+import bp.model.CategoryType;
+import bp.model.Summary;
 import bp.repository.SummaryRepository;
 import bp.repository.TransactionRepository;
 import bp.services.*;
 
 import javax.swing.*;
+import java.time.LocalDate;
 
 /**
  * Created by agnieszka on 16.06.2017.
@@ -61,10 +65,16 @@ public class MainWindow extends JFrame {
         }
 
         TransactionService transactionService = new TransactionService(transactionRepository);
-        SummaryService summaryService = new SummaryService(summaryRepository);
+        SummaryService summaryService = new SummaryService(summaryRepository, transactionRepository);
         GraphService graphService = new GraphService(summaryRepository);
         BudgetPlanner budgetPlanner = new BudgetPlanner(summaryRepository);
 
+
+        Summary exampleSummary = new Summary(LocalDate.now());
+        for (CategoryType categoryType : CategoryType.values()) {
+            exampleSummary.addExpense(new CategoryExpensesType(LocalDate.now(), categoryType, 1));
+        }
+        summaryService.addSummary(exampleSummary);
 
         MainWindow window = new MainWindow(transactionService, summaryService, graphService, budgetPlanner);
 
--- src/bp/gui/summary/SummaryPieChart.java
+++ src/bp/gui/summary/SummaryPieChart.java
@@ -1,5 +1,6 @@
 package bp.gui.summary;
 
+import bp.model.CategoryType;
 import bp.services.GraphService;
 import org.jfree.chart.ChartFactory;
 import org.jfree.chart.ChartPanel;
@@ -12,6 +13,9 @@ import org.jfree.ui.RectangleEdge;
 import org.jfree.util.Rotation;
 
 import javax.swing.*;
+import java.time.LocalDate;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by agnieszka on 18.06.2017.
@@ -21,25 +25,26 @@ public class SummaryPieChart extends JPanel {
 
     public SummaryPieChart(String chartTitle, GraphService graphService) {
         this.graphService = graphService;
-        PieDataset dataset = createDataset();
+        PieDataset dataset = createDataset(graphService);
         JFreeChart chart = createChart(dataset, chartTitle);
         ChartPanel chartPanel = new ChartPanel(chart);
         add(chartPanel);
-
-
     }
 
-    private PieDataset createDataset() {
+    private PieDataset createDataset(GraphService graphService) {
         DefaultPieDataset result = new DefaultPieDataset();
-        result.setValue("Food", 10);
-        result.setValue("Cos tam1", 40);
-        result.setValue("Cos tam2", 50);
+
+        Map<CategoryType, Double> dataset = graphService.pieChart(LocalDate.now());
+        for (CategoryType category : CategoryType.values()) {
+            result.setValue(category.name(), dataset.get(category));
+        }
+
         return result;
     }
 
-
     private JFreeChart createChart(PieDataset dataset, String title) {
         JFreeChart chart = ChartFactory.createPieChart(title, dataset, true, true, false);
+
         LegendTitle legend = chart.getLegend();
         legend.setPosition(RectangleEdge.RIGHT);
         PiePlot plot = (PiePlot) chart.getPlot();
@@ -47,8 +52,6 @@ public class SummaryPieChart extends JPanel {
         plot.setDirection(Rotation.CLOCKWISE);
         plot.setForegroundAlpha(0.5f);
         return chart;
-
-
     }
 
 
